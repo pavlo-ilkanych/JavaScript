db43b11fe51b81fc5f0cfb5405a38ee46df1c6f1
pavlo-ilkanych <pasha.ilkanich@gmail.com>
2021-04-26 16:17:43 +0300

    Other task

---
+++ Homework 6/Lesson1/script.js
@@ -0,0 +1,29 @@
+//Task 1
+let city;
+city = "Kyiv";
+city = "Lviv";
+let address = city;
+alert(address);
+
+//Task 2
+let n = 10;
+let result;
+result = Math.pow(n, 3);
+// rusul = n ** 3; або так
+console.log(result);
+
+//Task 3
+let pen = 4;
+let penPrice = 5.25;
+let pencil = 6;
+let pencilPrice = 3.50;
+let totalCost = (pen*penPrice) + (pencil*pencilPrice);
+document.write(totalCost);
+
+//Task 4
+let num1 = 2;
+let num2 = 6;
+let num3 = 8
+let rslt = 24;
+let x = (num2/num1) * (rslt - num3);
+console.log(x);
--- Homework 6/script.js
+++
@@ -1,6 +0,0 @@
-//Task 1
-let city;
-city = "Kyiv";
-city = "Lviv";
-let address = city;
-alert(address);
